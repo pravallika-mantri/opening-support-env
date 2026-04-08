fa7f9fb913e3d1dd62cb94d06f7b8e36051526a4
Jaya Pravallika Mantri <pravallikamantri@users.noreply.huggingface.co>
2026-04-08 16:03:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,13 @@ app = FastAPI()
 
 @app.get("/")
 def run_env():
-    # Run your inference script
     result = subprocess.run(
         ["python", "inference.py"],
-        capture_output=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
         text=True
     )
-    return {"output": result.stdout}
+
+    output = result.stdout if result.stdout else result.stderr
+
+    return {"output": output}
